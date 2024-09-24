5e4a5eb81d5c67f77b8a803ce2036988c4167912
dag <45577604+dag25@users.noreply.github.com>
2024-09-24 17:50:59 +0400

    feat: lesson03

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,34 @@ console.log(multiplication);
 const exponentiation = multiplication ** 3;
 console.log(exponentiation);
 console.log(exponentiation.toString().substring(0, 2));
+
+let lang = 'en';
+let daysEn = 'monday, tuesday, wednesday, thursday, friday, saturday, sunday';
+let daysRu = 'понедельник, вторник, среда, четверг, пятница, суббота, воскресенье';
+
+if (lang === 'en') {
+	console.log(daysEn);
+}else {
+	console.log(daysRu);
+}
+switch (lang) {
+	case 'en':
+		console.log(daysEn);
+		break;
+
+	case 'ru':
+		console.log(daysRu);
+		break;
+}
+
+const arr = [];
+arr.ru = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'];
+arr.en = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+console.log(arr[lang]);
+
+const namePerson = 'Артем';
+namePerson === 'Артем'
+	? console.log('директор')
+		: namePerson === 'Александр'
+		? console.log('преподаватель')
+	: console.log('студент');
